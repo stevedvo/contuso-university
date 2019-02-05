31e2f71f01e5c55d6baa49c6e38910b8dce10981
Steve DeVeaux <steve.deveaux@kloc.co.uk>
2019-02-05 11:20:28 +0000

    - add FullName property to Student; add validation to Student properties; add migrations

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,13 +10,27 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
-		[StringLength(50)]
+		[StringLength(50, MinimumLength = 1)]
+		[Display(Name = "Last Name")]
         public string LastName { get; set; }
+		[Required]
 		[StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters")]
-        public string FirstMidName { get; set; }
+		[Column("FirstName")]
+		[Display(Name = "First Name")]
+		public string FirstMidName { get; set; }
 		[DataType(DataType.Date)]
 		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime EnrolmentDate { get; set; }
+		[Display(Name = "Enrolment Date")]
+		public DateTime EnrolmentDate { get; set; }
+
+		[Display(Name = "Full Name")]
+		public string FullName
+		{
+			get
+			{
+				return LastName + ", " + FirstMidName;
+			}
+		}
 
         public virtual ICollection<Enrolment> Enrolments { get; set; }
     }
